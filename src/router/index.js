eb74d09a05f10d1b32008cc5b63f2112608fef7a
hugewilliam <hugewilliam@foxmail.com>
2020-03-01 22:47:05 +0800

    更改webpack配置

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 
 // 这里的页面组件还没有编辑，先占下位置
-import Login from '../views/Login';
-import Chat from '../views/Chat';
+import Login from '@/views/Login';
+import Chat from '@/views/Chat';
 
 export default class Router extends React.Component{
     render(){
