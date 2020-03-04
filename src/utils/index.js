32c538a92bd3475121733d6bb60c3206debb55c7
hugewilliam <hugewilliam@foxmail.com>
2020-03-05 01:01:07 +0800

    添加两个作用于cookie的工具函数

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,3 +7,26 @@
 export const changeTitle = (title = '摸鱼俱乐部') => {
     document.title = title;
 }
+
+/**
+ * 获取当前cookie数据对象
+ */
+export const getCookieObj = () => {
+    const cookies = document.cookie.split(';');
+    return cookies.reduce((total, cur) => {
+        const [key, value] = cur.split('=');
+        return {
+            ...total,
+            [key]: value
+        }
+    }, {})
+}
+
+/**
+ * 获取cookie数据
+ * @key     String  cookie的键名
+ */
+export const getCookie = (key) => {
+    const cookies = getCookieObj();
+    return cookies[key];
+}
