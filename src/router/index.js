3511177027d09cdd148db709636d018dead19d84
hugewilliam <hugewilliam@foxmail.com>
2020-03-05 01:35:44 +0800

    增加登录权限拦截

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 
 // 这里的页面组件还没有编辑，先占下位置
 import Login from '@/views/Login';
 import Chat from '@/views/Chat';
+
+// 引入cookie工具函数
+import {getCookie} from 'utils';
 
 export default class Router extends React.Component{
     render(){
@@ -12,8 +15,30 @@ export default class Router extends React.Component{
                 {/* 登录/注册 */}
                 <Route exact path="/login" component={Login}/>
                 {/* 聊天界面 */}
-                <Route exact path="/chat" component={Chat}/>
+                <PrivateRouter path="/">
+                    <Chat/>
+                </PrivateRouter>
             </Switch>
         )
     }
+}
+
+// 登录权限控制
+function PrivateRouter({children, ...props}){
+    return (
+        <Route
+            {...props}
+            render={({location}) => {
+                const hasAuthority = getCookie('username');
+                return (
+                    hasAuthority ? 
+                    children : 
+                    <Redirect to={{
+                        pathname: '/login',
+                        state: {from: location}
+                    }}/>
+                )
+            }}
+        />
+    )
 }
