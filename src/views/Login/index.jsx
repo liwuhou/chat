fa53f709a116757a4158d938c5846a1d0e56d358
hugewilliam <hugewilliam@foxmail.com>
2020-03-10 22:50:09 +0800

    修改登陆页面，并编写样式

--- src/views/Login/index.jsx
+++ src/views/Login/index.jsx
@@ -1,7 +1,134 @@
 import React from 'react';
+import Alert from '@/components/Alert';
 
-export default function Login(){
-    return (
-        <div>login</div>
-    )
+import {login} from '@/api'
+import './index.scss';
+
+export default class Login extends React.Component{
+    constructor(props){
+        super(props);
+		// 这里需要获取到路由跳转的from信息，好让我们登录的时候回到上一个页面
+        const {from} = this.props.location.state || {from: {pathname: '/'}}
+        this.state = {
+            from,
+            username: '',
+            password: '',
+            // 弹框信息
+            AlertProps: {
+                isShow: false,
+                title: '提示',
+                description: '',
+                onConfirm: this.onAlertConfirm,
+            },
+        }
+    }
+
+    // 输入用户名
+    handleChangeUsername = (event) => {
+        const username = event.target.value.replace(/\s+/, '');
+        this.setState({
+            username
+        })
+    }
+    
+    // 输入密码
+    handleChangePawsword = (event) => {
+        const password = event.target.value;
+        this.setState({
+            password
+        })
+    }
+
+    handleKeyUpLogin = (event) => {
+        if(event.keyCode === 13) return this.handleClickSubmitUserData();
+    }
+
+    // 进入
+    handleClickSubmitUserData = async () => {
+        const {username, password} = this.state;
+        if(!username) return this.showAlert('请输入摸鱼者大名！');
+        if(!password) return this.showAlert('请输入摸鱼口令！');
+        if(username.length < 2) return this.showAlert('行走江湖名号怎可少于两个字！');
+        if(username.length > 8) return this.showAlert('摸鱼人的大名再大也大不过八个字！');
+        if(password.length < 6) return this.showAlert('口令太短，阁下请重新输过！');
+        if(password.length > 16) return this.showAlert('口令这么长，我都记不过来！');
+
+        console.log("Login -> handleClickSubmitUserData -> login", login)
+        const data = await login({username, password});
+        console.log("Login -> handleClickSubmitUserData -> data", data)
+        if(data.status !== 1) return this.showAlert(data.message);
+        
+        // 模拟请求成功后，响应头写入cookie的操作
+        document.cookie='username=willliam';
+        
+        // 验证成功，跳转回前一个页面
+        this.props.history.replace(this.state.from);
+    }
+
+    // 弹框展示
+    showAlert(description, title = '提示'){
+        this.setState(({AlertProps}) => ({
+            AlertProps: {
+                ...AlertProps,
+                isShow: true,
+                title,
+                description
+            }
+        }))
+    }
+
+    // 弹框中的确定
+    onAlertConfirm = () => {
+        this.setState(({AlertProps}) => ({
+            AlertProps: {
+                ...AlertProps,
+                isShow: false,
+                description: ''
+            }
+        }))   
+    }
+
+    render(){
+        return (
+            <div className="login">
+                <img className="login__sologan" src="./sologan.svg" alt="武陵人摸鱼为业"/>
+                <div className="login__input">
+                    <label htmlFor="username" className="login__input_label">摸鱼人：</label>
+                    <input
+                        type="text"
+                        id="username" 
+                        placeholder="请输入昵称"
+                        value={this.state.username}
+                        onKeyUp={this.handleKeyUpLogin}
+                        onChange={this.handleChangeUsername}
+                    />
+                </div>
+                <div className="login__input">
+                    <label htmlFor="password" className="login__input_label">摸鱼口令：</label>
+                    <input 
+                        id="password" 
+                        type="password" 
+                        placeholder="请输入密码"
+                        value={this.state.password} 
+                        onKeyUp={this.handleKeyUpLogin}
+                        onChange={this.handleChangePawsword}
+                    />
+                    <i 
+                        className="login__info"
+                        onClick={() => this.showAlert(`
+                            初次进入将会注册昵称和账号，后续进入只需要输入已注册的账号和口令即可。
+                        `)}></i>
+                </div>
+                <div className="login__submit">
+                    <input
+                        value="进 入" 
+                        type="button" 
+                        onClick={this.handleClickSubmitUserData}
+                    />
+                </div>
+
+                <Alert {...this.state.AlertProps}/>
+            </div>
+        )
+    }
 }
