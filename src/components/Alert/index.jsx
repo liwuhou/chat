b693436ad904c74db5fe794239cc6ad830596a7c
hugewilliam <hugewilliam@foxmail.com>
2020-03-10 22:47:57 +0800

    封装弹框组件

---
+++ src/components/Alert/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './index.scss';
+
+export default function Alert({isShow, title = '提示', description = '', confirmText = '好的吧', cancelText = '算了', onConfirm, onCancel = null}){
+    return (
+        <React.Fragment>
+            {
+                isShow ? ( 
+                    <div className="alert-cover">
+                        <div className="alert">
+                            <div className="alert__heading">{title}</div>
+                            <div className="alert__description">{description}</div>
+                            <div className="alert__buttons">
+                                {onCancel && <button className="alert__buttons_cancel" onClick={onCancel}>{cancelText}</button>}
+                                <button className="alert__buttons_confirm" onClick={onConfirm}>{confirmText}</button>
+                            </div>
+                        </div>
+                    </div>
+                ) : null
+            }
+        </React.Fragment>
+    )
+}
