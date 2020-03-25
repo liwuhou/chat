6766a9654fd2ca72c4bc60c7a8ad1d8590f755a0
hugewilliam <hugewilliam@foxmail.com>
2020-03-26 07:47:25 +0800

    使用React Hooks重构输入框组件

--- src/components/ChatInput/index.jsx
+++ src/components/ChatInput/index.jsx
@@ -1,43 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './index.scss';
 
-class ChatInput extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            message: '',
-        }
-    }
-    handleKeyUpMessage = (event) => {
-        if(event.keyCode === 13) this.handleClickSendMessage();
-    }
-    handleClickSendMessage = () => {
-        if(!this.state.message) return;
-        this.props.onsendMsg(this.state.message);
-        this.setState({message: ''});
-    }
-    handleChangeMessage = (event) => {
-        this.setState({
-            message: event.target.value
-        });
-    }
+// export default class ChatInput extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { 
+//             message: '',
+//         }
+//     }
+//     handleKeyUpMessage = (event) => {
+//         if(event.keyCode === 13) this.handleClickSendMessage();
+//     }
+//     handleClickSendMessage = () => {
+//         if(!this.state.message) return;
+//         this.props.onsendMsg(this.state.message);
+//         this.setState({message: ''});
+//     }
+//     handleChangeMessage = (event) => {
+//         this.setState({
+//             message: event.target.value
+//         });
+//     }
     
-    render() { 
-        return ( 
-            <div className="chat__input">
-                <input
-                    className="chat__input_white"
-                    value={this.state.message}
-                    onChange={this.handleChangeMessage}
-                    onKeyUp={this.handleKeyUpMessage}
-                />
-                <button
-                    className="chat__input_btn"
-                    onClick={this.handleClickSendMessage}
-                >发送</button>
-            </div>
-        );
-    }
-}
+//     render() { 
+//         return ( 
+//             <div className="chat__input">
+//                 <input
+//                     className="chat__input_white"
+//                     value={this.state.message}
+//                     onChange={this.handleChangeMessage}
+//                     onKeyUp={this.handleKeyUpMessage}
+//                 />
+//                 <button
+//                     className="chat__input_btn"
+//                     onClick={this.handleClickSendMessage}
+//                 >发送</button>
+//             </div>
+//         );
+//     }
+// }
  
-export default ChatInput;
+export default function ChatInput({onSendMessage}){
+	const [message, setMessage] = useState('');
+	// 输入框回车发送
+	function handleKeyUpMessage(event){
+		if(event.keyCode === 13) return handleClickSendMessage;
+	}
+
+	// 发送按钮
+	function handleClickSendMessage(){
+		if(!message) return;
+		onSendMessage(message);
+		setMessage('');
+	}
+	return (
+		<div className="chat__input">
+			<input
+				className="chat__input_white"
+				value={message}
+				onChange={({target:{value}}) => setMessage(value)}
+				onKeyUp={handleKeyUpMessage}
+			/>
+			<button
+				className="chat__input_btn"
+				onClick={handleClickSendMessage}
+			>发送</button>
+		</div>
+	)
+}
