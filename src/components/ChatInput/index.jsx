a6a874f827de37429c9faaf0c488006e09b9129d
hugewilliam <hugewilliam@foxmail.com>
2020-03-26 07:48:05 +0800

    删除无用注释

--- src/components/ChatInput/index.jsx
+++ src/components/ChatInput/index.jsx
@@ -1,44 +1,6 @@
 import React, { useState } from 'react';
 import './index.scss';
 
-// export default class ChatInput extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = { 
-//             message: '',
-//         }
-//     }
-//     handleKeyUpMessage = (event) => {
-//         if(event.keyCode === 13) this.handleClickSendMessage();
-//     }
-//     handleClickSendMessage = () => {
-//         if(!this.state.message) return;
-//         this.props.onsendMsg(this.state.message);
-//         this.setState({message: ''});
-//     }
-//     handleChangeMessage = (event) => {
-//         this.setState({
-//             message: event.target.value
-//         });
-//     }
-    
-//     render() { 
-//         return ( 
-//             <div className="chat__input">
-//                 <input
-//                     className="chat__input_white"
-//                     value={this.state.message}
-//                     onChange={this.handleChangeMessage}
-//                     onKeyUp={this.handleKeyUpMessage}
-//                 />
-//                 <button
-//                     className="chat__input_btn"
-//                     onClick={this.handleClickSendMessage}
-//                 >发送</button>
-//             </div>
-//         );
-//     }
-// }
  
 export default function ChatInput({onSendMessage}){
 	const [message, setMessage] = useState('');
